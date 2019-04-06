f466fe38d52f281541f8e10dcf635a53100fd954
fidilly <fidship@hotmail.com>
2019-04-06 12:14:01 +0400

    Updating Progression game

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -6,22 +6,20 @@ use function BrainGames\Engine\startGame;
 
 const GAME_DESCRITPION = 'What number is missing in the progression?';
 const PROGRESSION_LENGTH = 10;
-const HIDESIGN = "..";
 
 function getGameData()
 {
     $generateData = function () {
         $startValue = rand(1, 10);
         $stepValue = rand(1, 10);
-        $progressionNumbers[] = $startValue;
 
-        for ($i = 1; $i < PROGRESSION_LENGTH; $i++) {
+        for ($i = 0; $i < PROGRESSION_LENGTH; $i++) {
             $progressionNumbers[] = $startValue + $stepValue * $i;
         }
 
         $indexToHide = array_rand($progressionNumbers);
         $correctAnswer = $progressionNumbers[$indexToHide];
-        $progressionNumbers[$indexToHide] = HIDESIGN;
+        $progressionNumbers[$indexToHide] = "..";
         $question = implode(" ", $progressionNumbers);
         
         return [$question, $correctAnswer];
